2045ea7f1bb6dbdc6886c5aece6099a9ac43141a
David Rash <DavidR@test.com>
2025-08-11 15:02:07 +0300

    create header and scss settings

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Header from "./components/header/Header.tsx";
+
+function App() {
+
+  return (
+      <>
+          <Header/>
+
+      </>
+  )
+}
+
+export default App
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,46 @@
+import text from "../../utils/text.json";
+import {useState} from "react";
+import "./Header.scss"
+
+const Header = () => {
+    const [carts, setCarts] = useState(0);
+
+    const listInfo =  text["header-nav-list"];
+    const listInfoAdditional =  text["header-nav-list-additional"];
+
+
+
+    return (
+        <header className="header">
+            <nav className="header-nav">
+                <ul className="nav-list">
+                    {
+                        listInfo.map((item, index) => (
+                            <li key={index} className="list-item">
+                                <a className="list-item-link" href="#">{item}</a>
+                            </li>
+                        ))
+                    }
+                </ul>
+
+                <div className={"nav-logo"}>
+                    <img className="nav-logo__img" src={"/public/images/logo-black-1.png.png"} alt={"logo"}
+                         width={166} height={38} loading={"lazy"}/>
+                </div>
+
+                <ul className="list-additional">
+                    {
+                        listInfoAdditional.map((item, index) => (
+                            <li key={index} className="list-item additional">
+                                <a className="list-item-link" href="">{item}{item === "cart" ? `(${carts})` : ""}</a>
+                            </li>
+                        ))
+                    }
+                </ul>
+
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
