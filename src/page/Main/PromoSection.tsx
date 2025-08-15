1b509723572fd70cd672512d5492bae79920477e
David Rash <DavidR@test.com>
2025-08-16 02:36:28 +0300

    some fix PromoSection.tsx

--- src/page/Main/PromoSection.tsx
+++ src/page/Main/PromoSection.tsx
@@ -4,6 +4,10 @@ import {useState} from "react";
 const PromoSection = () => {
     const productsArray =products.products.slice(0,2)
     const [indexQuotes, setIndexQuotes] = useState(0);
+    const handleClick = (index:number)=>{
+        //todo event?
+        setIndexQuotes(index)
+    }
 
 
     return (
@@ -21,6 +25,7 @@ const PromoSection = () => {
             <section className="product">
                 <div className="product-info">
                     <h3 className="product-title">Our blends deliver on the promise of <br/> balance and consistency,<span>roasted to</span> <br/> maximize sweetness.</h3>
+                    {/*todo*/}
                     <p className="product-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut <br/> elit tellus, luctus nec ullamcorper.</p>
                     <button className="product-button more-info">View More</button>
                 </div>
@@ -44,31 +49,34 @@ const PromoSection = () => {
             </section>
             <section className="quotes">
                 <div className="quotes-body">
-                    <h4 className="quotes-text"></h4>
-                    <p className="quotes-owners"></p>
+                    <h3 className="quotes-text">"Another reason people just can't keep away from their local coffee shop is the quality of coffee that's on offer. "</h3>
+                    <p className="quotes-owners">Henry Monro</p>
                     <ul className="quotes-list">
                         <li className="quotes-item">
                             <button
                                 className="quotes-button"
                                 type="button"
-                                onClick={()=>setIndexQuotes(0)}
+                                onClick={()=>handleClick(0)}
                             ></button>
                         </li>
                         <li className="quotes-item">
                             <button
                                 className="quotes-button "
                                 type="button"
+                                onClick={()=>handleClick(1)}
                             ></button>
                         </li>
                         <li className="quotes-item">
                             <button
                                 className="quotes-button is-current"
                                 type="button"
+                                onClick={()=>handleClick(2)}
                             ></button>
                         </li>
                     </ul>
                 </div>
                 <img className="quotes-image" src="/public/images/cap-of-coffee.png" alt="images with cap of coffee"
+
                      loading="lazy"/>
             </section>
         </>
