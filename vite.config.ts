920a699f88116242a55aeb2a2c62e5b10e6960dc
davidr <davidrashkovskiy@gmail.com>
2025-10-20 15:35:23 +0300

    try to host

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/coffee-shop-ui/', // путь соответствует имени репозитория
+  base: '/coffee-shop-ui/', // название репозитория
   plugins: [react()],
 })
+
